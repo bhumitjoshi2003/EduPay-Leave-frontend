262bdc61ca6f014c3cc06e322f401640e2391c61
Priyanshu Joshi <priyanshujoshi@Priyanshus-MacBook-Air-2.local>
2025-03-11 11:48:29 +0530

    added fee-structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,14 @@ import { provideRouter, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { StudentComponent } from './components/student/student.component';
 import { PaymentTrackerComponent } from './components/fees/fees.component';
+import { FeeStructureComponent } from './components/fee-structure/fee-structure.component';
 
 // import { AuthGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full'},
     { path: 'home', component: HomeComponent},
-    // { path: 'student', component: StudentComponent, canActivate:[AuthGuard] },
+    { path: 'fee-structure', component: FeeStructureComponent },
     { path: 'fees', component: PaymentTrackerComponent },
     { path: '**', redirectTo: '/home', pathMatch: 'full' },
 ];

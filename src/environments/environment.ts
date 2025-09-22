71c8c1b1f3054c48ad40a1668928aea5cf6091c9
Priyanshu Joshi <priyanshujoshi@Priyanshus-MacBook-Air-2.local>
2025-09-22 23:03:51 +0530

    Added netlify.toml file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://13.233.93.234:8081'
-    // apiUrl: 'https://ias-management-images-latest.onrender.com'
-    // apiUrl: 'http://192.168.1.2:8081'
+    apiUrl: '/api'
 };
 

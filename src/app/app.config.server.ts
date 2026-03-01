5f52ef5dc5b83aec69796671e692b88cb2489d97
Bhumit Joshi <bhumitjoshi2003@gmail.com>
2026-03-01 18:43:05 +0530

    Updated app.config.server

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,10 +1,14 @@
-import { provideServerRendering, withRoutes } from '@angular/ssr';
+import { provideServerRouting, ServerRoute } from '@angular/ssr';
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { appConfig } from './app.config';
 import { serverRoutes } from './app.routes.server';
 
 const serverConfig: ApplicationConfig = {
-  providers: [provideServerRendering(withRoutes(serverRoutes))]
+  providers: [provideServerRouting(withRoutes(serverRoutes))]
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
+function withRoutes(serverRoutes: ServerRoute[]): import("@angular/ssr").ServerRoute[] {
+  throw new Error('Function not implemented.');
+}
+

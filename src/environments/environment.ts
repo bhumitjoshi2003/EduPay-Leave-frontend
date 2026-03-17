e1ac59d5500b59cb8c907841de265e051abcca9c
Bhumit Joshi <bhumitjoshi2003@gmail.com>
2026-03-17 23:51:38 +0530

    Updated backend path

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     // apiUrl: 'http://ec2-15-206-89-113.ap-south-1.compute.amazonaws.com:8080'
-    apiUrl: 'http://localhost:8080/api'
+    apiUrl: '/api'
 };
